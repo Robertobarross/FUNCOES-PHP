c6dc6f4d2b488bc28e975e64a2539cc68104f3a8
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-04-12 22:03:00 -0300

    Create index.php

---
+++ decimal-hexadecimal-php/index.php
@@ -0,0 +1,14 @@
+<?php
+
+function decimalHexadecimal(){
+
+    // Decimal para hexadecimal
+    echo dechex('3549') . "\n";
+    echo dechex('6625') . "\n";
+    echo dechex('9579') . "\n";
+
+}
+$resultado = decimalHexadecimal();
+return $resultado;
+
+?>
